f8bc36914a801f399254801f19911250a0d74244
nallamsrikanth12 <144240956+nallamsrikanth12@users.noreply.github.com>
2023-09-07 14:19:21 +0530

    Update marolix.py

--- marolix.py
+++ marolix.py
@@ -1,1 +1,10 @@
 marolix company is a sucessfully innovate
+echo "enter the username"
+read username
+echo "enter the password"
+read password
+if [[ ( $username == "marolix" && $password == "marolix.srikanth" ) ]] then
+echo "valid user"
+else
+echo "invalid user"
+fi
